55a421807027b212cf382a18847c422d32376a5b
Zachary Massia <zmassia@gmail.com>
2014-06-04 12:07:19 -0400

    Fix project link

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cljs-app/lein-template "0.1.0"
   :description "A simple ClojureScript app template using Weasel and Simpleton"
-  :url "github.com/zachmassia/cljs-app"
+  :url "https://github.com/zachmassia/cljs-app"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
