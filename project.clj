60d92bb3a6f8c883bbfe1a07142a47d7c733312d
Zachary Massia <zmassia@gmail.com>
2014-06-04 11:24:21 -0400

    Add scm map

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "github.com/zachmassia/cljs-app"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/zachmassia/cljs-app"}
   :eval-in-leiningen true)
